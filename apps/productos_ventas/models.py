c9b207868b6212f43ba9af039bbded234c604cf9
gakmc <gak335@gmail.com>
2019-11-07 21:23:48 -0400

    Nuevos Cambios

--- apps/productos_ventas/models.py
+++ apps/productos_ventas/models.py
@@ -1,5 +1,4 @@
 from django.db import models
-from ventas.models import models
 
 # Create your models here.
 
@@ -8,12 +7,7 @@ class productos_ventas(models.Model):
 
 
     cantidad = models.IntegerField()
-<<<<<<< HEAD
     venta_id = models.AutoField(Foreing_key=True)
     producto_id = models.AutoField(Foreing_key=True)
-=======
-    venta_id = models.Foreingkey("venta", on_delete=models.CASCADE)
-    producto_id = models.Foreingkey("Productos", on_delete=models.CASCADE)
->>>>>>> c2cf742cd0a8b70ff7f1fb5e9050f534e178fe38
 
 
