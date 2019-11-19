c4cf1e88db36317987aab14553743d0349a3ee8c
gakmc <gak335@gmail.com>
2019-11-19 10:49:50 -0400

    Se incorpora admin.py

--- apps/ventas/admin.py
+++ apps/ventas/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
-
+from apps.ventas.models import Venta,Productos_ventas
 # Register your models here.
+admin.site.register(Venta)
+admin.site.register(Productos_ventas)
