35479f26ad017a70afe19fb4ed864eb23269cd37
gakmc <gak335@gmail.com>
2019-11-19 20:21:34 -0400

    se modifico migrations y models venta se configuro

--- apps/ventas/models.py
+++ apps/ventas/models.py
@@ -6,7 +6,7 @@ from apps.productos.models import Producto
 class Venta(models.Model):
     
     fecha = models.DateField(auto_now_add=True)
-    comentarios = models.TextField(null = False, blank=False)
+    comentarios = models.TextField(null = True, blank=True)
     vendedor = models.ForeignKey(Vendedor,on_delete=models.CASCADE)
     producto = models.ManyToManyField(Producto, through='Productos_ventas')
 
